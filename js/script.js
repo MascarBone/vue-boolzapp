66bf1ed96b025998dbf34080d12b21204b343381
MascarBone <simone.bonelli@gmail.com>
2021-09-17 12:56:01 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,105 +1,3 @@
-const contacts = [
-    {
-        name: 'Michele',
-        avatar: '_1',
-        visible: true,
-        messages: [
-            {
-                date: '10/01/2020 15:30:55',
-                text: 'Hai portato a spasso il cane?',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 15:50:00',
-                text: 'Ricordati di dargli da mangiare',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 16:15:22',
-                text: 'Tutto fatto!',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Fabio',
-        avatar: '_2',
-        visible: true,
-        messages: [
-            {
-                date: '20/03/2020 16:30:00',
-                text: 'Ciao come stai?',
-                status: 'sent'
-            },
-            {
-                date: '20/03/2020 16:30:55',
-                text: 'Bene grazie! Stasera ci vediamo?',
-                status: 'received'
-            },
-            {
-                date: '20/03/2020 16:35:00',
-                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                status: 'sent'
-            }
-        ],
-    },
-
-    {
-        name: 'Samuele',
-        avatar: '_3',
-        visible: true,
-        messages: [
-            {
-                date: '28/03/2020 10:10:40',
-                text: 'La Marianna va in campagna',
-                status: 'received'
-            },
-            {
-                date: '28/03/2020 10:20:10',
-                text: 'Sicuro di non aver sbagliato chat?',
-                status: 'sent'
-            },
-            {
-                date: '28/03/2020 16:15:22',
-                text: 'Ah scusa!',
-                status: 'received'
-            }
-        ],
-    },
-    {
-        name: 'Luisa',
-        avatar: '_4',
-        visible: true,
-        messages: [
-            {
-                date: '10/01/2020 15:30:55',
-                text: 'Lo sai che ha aperto una nuova pizzeria?',
-                status: 'sent'
-            },
-            {
-                date: '10/01/2020 15:50:00',
-                text: 'Si, ma preferirei andare al cinema',
-                status: 'received'
-            }
-        ],
-    },
-];
-
-// const arrayNew = [];
-
-// for (const element of contacts) {
-//     const {name,avatar,visible,messages} = element;
-//     console.log(name,avatar,visible,messages);
-//     // const obj = {name,avatar,visible,messages};
-//     // console.log(obj);
-//     arrayNew.push({name,avatar,visible,messages,});
-// }
-// console.table(arrayNew);
-
-
-
-
-
 const app = new Vue({
     el: '#app',
 
@@ -190,25 +88,11 @@ const app = new Vue({
                 ],
             },
         ],
-
-        contactsCopy : [],
-
+        
     },
 
     methods: {
 
     },
 
-    created: function () {
-        this.contacts.forEach(function(element) {
-            const {name,avatar,visible,messages} = element;
-            console.log(name,avatar,visible,messages);
-            // const obj = {name,avatar,visible,messages};
-            // console.log(obj);
-            this.contactsCopy.push({name,avatar,visible,messages});
-            // this.contactss.push({name,avatar,visible,messages,});
-            // console.log(this.contactss);
-        });
-        console.table(this.contactsCopy);le(this.contactss);
-    },
 })
